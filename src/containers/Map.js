b428272fa6f3f7417072bf1ac55c795778a894ab
Julian Miqueo <jmiqueo@fortinet-us.com>
2017-01-27 12:54:33 -0800

    maps

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -15,66 +15,62 @@ class Map extends React.Component {
   }
 
   componentDidMount() {
-    var width = 450,
-        height = 450;
+    let width = 450,
+      height = 450;
 
     // set projection
-    var projection = d3.geo.mercator();
+    let projection = d3.geo.mercator();
 
     // create path variable
-    var path = d3.geo.path()
-        .projection(projection);
+    let path = d3.geo.path()
+      .projection(projection);
 
+    d3.json("/json/Bay_Area_Cities_topo.json", function(error, topo) {
+      console.log('topo', topo);
+    	let zones = feature(topo, topo.objects.Bay_Area).features;
+console.log('zones', zones);
+    	// set projection parameters
+    	projection
+        .scale(30000)
+        .center([-121.7, 37.5]);
 
-    d3.json("/json/Bay_Area_Cities_topo.json", function(error, topo) { console.log(topo);
-console.log('feature', feature);
-      	var states = feature(topo, topo.objects.Bay_Area).features
+      // create svg variable
+      let svg = d3.select(".map").append("svg")
+				.attr("width", width)
+				.attr("height", height);
 
-      	// set projection parameters
-      	projection
-          .scale(30000)
-          .center([-121.7, 37.5])
-
-        // create svg variable
-        var svg = d3.select("body").append("svg")
-        				.attr("width", width)
-        				.attr("height", height);
-
-        // points
-        var aa = [-122.5, 37.5];
-    	var bb = [-122.6, 37.5];
-
-    	console.log(projection(aa),projection(bb));
-
-    	// add states from topojson
+    	// add zones from topojson
     	svg.selectAll("path")
-          .data(states).enter()
-          .append("path")
-          .attr("class", "feature")
-          .style("fill", "steelblue")
-          .attr("d", path);
+        .data(zones).enter()
+        .append("path")
+        .attr("class", "feature")
+        .style("fill", "steelblue")
+        .attr("d", path);
 
-        // put boarder around states
-      	// svg.append("path")
-        //   .datum(topojson.mesh(topo, topo.objects.states, function(a, b) { return a !== b; }))
-        //   .attr("class", "mesh")
-        //   .attr("d", path);
-
-        // add circles to svg
-        svg.selectAll("circle")
-    		.data([aa,bb]).enter()
-    		.append("circle")
-    		.attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
-    		.attr("cy", function (d) { return projection(d)[1]; })
-    		.attr("r", "8px")
-    		.attr("fill", "red")
-
+      fetch(`/json/stops.json`)
+        .then(function(response) {
+          return response.json()
+        }).then(function(json) {
+          let a = [];
+          Object.keys(json).forEach(function(name) {
+            a.push([json[name][3],json[name][2]]);
+          });
+          svg.selectAll("circle")
+            .data(a).enter()
+            .append("circle")
+            .attr("cx", function (d) { console.log('pro', projection(d)); return projection(d)[0]; })
+            .attr("cy", function (d) { return projection(d)[1]; })
+            .attr("r", "2px")
+            .attr("fill", "#666");
+        }.bind(this)).catch(function(ex) {
+          console.log('parsing failed', ex)
+        });
     });
   }
 
   render() {
     return(
-      <div>Map</div>
+      <div className="map"></div>
     );
   }
 }
